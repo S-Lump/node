2bc07100094a7dbb686e38cf9a4a96797c64cc5a
StupidLamer <grisga2001@gmail.com>
2021-01-05 04:29:58 +0300

    add base express config

--- expressJS/index.js
+++ expressJS/index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
 
 const PORT = process.env.PORT || 3000
 

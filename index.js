d2ce598288a87d8b07e39819671e06accbe9197e
StupidLamer <grisga2001@gmail.com>
2021-01-05 03:10:56 +0300

    create simple echo server

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log(req.url)
+  if (req.method === 'GET') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    res.end(`
+      <h1>Form</h1>
+      <form method="post" actions="/">
+        <input name="title" type="text" />
+        <button type="submit">Send</button>
+      </form>
+    `)
+  } else if (req.method === 'POST') {
+    const body = []
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    })
 
-  res.write('Hello from NodeJS')
-  res.end()
+    req.on('data', data => {
+      body.push(Buffer.from(data))
+    })
+
+    req.on('end', () => {
+      const message = body.toString().split('=')[1]
+
+      res.end(`
+        <h1>Your message: ${message}</h1>
+      `)
+    })
+  }
 })
 
 server.listen(3000, () => {

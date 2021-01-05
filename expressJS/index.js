1603d86e8da661d81d2aaba7c3bb62db8dd47250
StupidLamer <grisga2001@gmail.com>
2021-01-05 06:25:20 +0300

    add partials

--- expressJS/index.js
+++ expressJS/index.js
@@ -1,8 +1,6 @@
-const { S_IFDIR } = require('constants')
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
-const path = require('path')
 
 const hbs = exphbs.create({
   defaultLayout: 'main',
@@ -12,6 +10,8 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')

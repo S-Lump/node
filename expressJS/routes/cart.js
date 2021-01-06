bcf40fcd7bcb9bbb1de51fc52322a8b15fb7e265
StupidLamer <grisga2001@gmail.com>
2021-01-06 15:33:43 +0300

    add cart

--- expressJS/routes/cart.js
+++ expressJS/routes/cart.js
@@ -6,14 +6,16 @@ const router = Router()
 router.post('/add', async (req, res) => {
   const course = await Course.getById(req.body.id)
   await Cart.add(course)
-  res.redirect('/card')
+  res.redirect('/cart')
 })
 
 router.get('/', async (req, res) => {
   const cart = await Cart.fetch()
   res.render('cart', {
     title: 'Корзина',
-    cart
+    isCart: true,
+    courses: cart.courses,
+    price: cart.price,
   })
 })
 
